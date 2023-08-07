85001438e213b029065b387539b1b91617746c4f
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-08-08 00:36:38 +0545

    added view inventory1 frontend part

--- client/src/components/inventory.js
+++ client/src/components/inventory.js
@@ -1,7 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Yup from 'yup';
-import { Button, Modal, message } from 'antd';
+import { Button, Modal, message, Avatar, Divider, List, Skeleton } from 'antd';
 import CustomFrom from './custom_form';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+const InventoryList = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const loadMoreData = () => {
+        if (loading) {
+            return;
+        }
+        setLoading(true);
+        fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
+            .then((res) => res.json())
+            .then((body) => {
+                setData([...data, ...body.results]);
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    };
+    useEffect(() => {
+        loadMoreData();
+    }, []);
+    return (
+        <div
+            id="scrollableDiv"
+            style={{
+                height: 690,
+                overflow: 'auto',
+                padding: '0 16px',
+                border: '1px solid rgba(140, 140, 140, 0.35)',
+            }}
+        >
+            <InfiniteScroll
+                dataLength={data.length}
+                next={loadMoreData}
+                hasMore={data.length < 50}
+                loader={
+                    <Skeleton
+                        avatar
+                        paragraph={{
+                            rows: 1,
+                        }}
+                        active
+                    />
+                }
+                endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                scrollableTarget="scrollableDiv"
+            >
+                <List
+                    dataSource={data}
+                    renderItem={(item) => (
+                        <List.Item key={item.email}>
+                            <List.Item.Meta
+                                avatar={<Avatar src={item.picture.large} />}
+                                title={<a href="https://ant.design">{item.name.last}</a>}
+                                description={item.email}
+                            />
+                            <div>Content</div>
+                        </List.Item>
+                    )}
+                />
+            </InfiniteScroll>
+        </div>
+    );
+};
 
 const Inventory = () => {
 
@@ -109,6 +176,7 @@ const Inventory = () => {
         </div>
         <hr ></hr>
         <div>
+            <InventoryList></InventoryList>
         </div>
     </div >
 }
